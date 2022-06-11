4ea0db5f22c29c9def7db987cb88bf7f9439c320
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-06-11 13:04:57 -0400

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    # split sentence on period, question mark, or exclamation mark
+    # count the number of elements from the split
+    found = self.split(/[\. \?+ !+] /)
+    found.length
   end
 end
+
